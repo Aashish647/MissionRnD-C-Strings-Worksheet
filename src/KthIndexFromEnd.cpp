a1112a8cba48462cc993c0029b19815c56cce558
Aashish647 <aashish647@gmail.com>
2015-12-09 12:54:25 +0530

    Submit Commit

--- src/KthIndexFromEnd.cpp
+++ src/KthIndexFromEnd.cpp
@@ -10,7 +10,34 @@ ERROR CASES: Return '\0' for invalid inputs.
 
 NOTES:
 */
+#include<stdlib.h>
+int findLen(char *);
+char KthIndexFromEnd(char *str, int K) 
+{
+	int len;
+	if ((str == NULL) || (K < 0))
+	{
+		return '\0';
+	}
+	len = findLen(str);
+	if (K > len)
+	{
+		return '\0';
+	}
+	len = len - 1;
+	for (; K != 0;K--,len--)
+	{ 
+	}
+	return str[len];
+	
+}
 
-char KthIndexFromEnd(char *str, int K) {
-	return '\0';
+int findLen(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return i;
 }
